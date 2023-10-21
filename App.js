10b089623f7e87efb3b6c8410809b5e6e2631fae
Vinay Barve <95542484+vinaybarve@users.noreply.github.com>
2023-10-21 17:05:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+
+import './App.css';
+
+import "./styles/styles.css"
+
+// import { history } from "./helpers/history";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import CategoriesPage from './pages/Categories/CategoriesPage';
+import ProductsPage from './pages/Products/ProductsPage';
+import NotAvailabePage from './pages/NotAvailabePage';
+import AddNewCategory from './pages/Categories/AddNewCategory';
+import AddNewProduct from './pages/Products/AddNewProduct'
+import Header from './components/Header';
+import RequestPage from './pages/Products/RequestPage';
+
+function App() {
+
+  return (
+
+    <main className='app'>
+  
+
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+          <Route exact path="/home" element={[<Header />, <HomePage />]} />
+          <Route exact path="/categories" element={[<Header />, <CategoriesPage />]} />
+          <Route exact path="/products" element={[<Header />, <ProductsPage />]} />
+          <Route exact path="/add-categories" element={[<Header />, <AddNewCategory />]} />
+          <Route exact path="/add-products" element={[<Header/>, <AddNewProduct/>]} />
+          <Route exact path="*" element={<NotAvailabePage />} />
+         
+        </Routes>
+      </BrowserRouter>
+      <RequestPage>
+        
+      </RequestPage>
+
+    </main>
+  )
+}
+
+export default App;
---
+++ RequestPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const RequestPage = () => {
+  return (
+    <div className='mainC'>
+        <h1 className='h'>Did you forget your password</h1>
+        <p>Enter your email address and we'will send you a link to respore password</p>
+
+        <p1 >Email address</p1><br></br>
+        <input></input>
+        <button className='req'>Request reset link</button>
+        <p>back to log in</p>
+    </div>
+  )
+}
+
+export default RequestPage
